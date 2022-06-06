77238d1efca10d0ddba8ff7f0a4ed050aae3dd6a
jmseca <joao.m.fonseca@gmail.com>
2022-06-06 16:04:52 +0100

    Added verified sql

--- Parte2/sql.sql
+++ Parte2/sql.sql
@@ -1,43 +1,55 @@
-4)
-SELECT Product.ean, Product.descr
-FROM Product as p natural join 
-	((SELECT ean, max_units
-	FROM (SELECT ean, sum(units)
-		FROM replenishment_event
-		GROUP BY ean)
-	WHERE max_units = max(units)))
+--1)
+
+SELECT
+    p.product_ean, p.descr
+FROM
+    product AS p
+INNER JOIN
+    replenishment_event AS r
+    ON r.product_ean=p.product_ean
+INNER JOIN
+    has AS h
+    ON h.product_ean=p.product_ean
+WHERE
+    r.instant > 4
+    AND r.units > 10
+    AND h.categ_name='Doces'
+
+--2)
+
+SELECT
+    p.serial_number AS IVM_Serial_Number
+FROM
+    planogram AS p
+WHERE
+    p.product_ean=234
+GROUP BY
+    p.serial_number
+
+--3)
+
+SELECT
+    COUNT(h.sub_category_name) AS sub_categ_count
+FROM
+    has_other AS h
+WHERE
+    h.super_category_name='Doces'
+GROUP BY
+    h.super_category_name
 
 
-SELECT Product.ean, Product.descr
-FROM Product as p inner join 
-	((SELECT ean, max_units
-	FROM (SELECT ean, sum(units)
-		FROM replenishment_event
-		GROUP BY ean)
-	WHERE max_units = max(units)) as aux_table)
-ON p.ean == aux_table.ean
+--4)
 
-3)
+WITH t1 AS (
+    SELECT p.product_ean, p.descr, SUM(r.units) AS units_sum
+	FROM product AS p
+    NATURAL JOIN replenishment_event AS r
+    GROUP BY p.product_ean)
 
-SELECT count(sub_category_name)		
-FROM has_other
-WHERE super_category_name = 'Sopas Take-Away'
-
-2)				(shelve é uma entidade fraca, portanto já tem lá o serial number)
-
-SELECT serial_number
-FROM planogram as p
-WHERE p.ean = '9002490100070'
-
-1)
-SELECT ean, descr
-FROM has natural join
-	(SELECT ean, descr
-	FROM Product natural join	
-		(SELECT ean, sum_of_units
-		FROM replenishment_event
-		WHERE instant > 2021/12/31 and sum_of_units = sum(units) and sum_of_units > 10
-		GROUP BY ean)
-	)
-WHERE name == 'Barras Energéticas'
-	
+SELECT
+    t1.product_ean, t1.descr
+FROM
+    t1
+WHERE
+    t1.units_sum = (SELECT MAX(units_sum)
+                    FROM t1)
