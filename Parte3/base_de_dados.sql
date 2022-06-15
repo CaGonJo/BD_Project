21c48b0f8b5de1582b31df874c0e7b31ccea540e
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-06-15 15:45:00 +0100

    populate part of the data base

--- Parte3/base_de_dados.sql
+++ Parte3/base_de_dados.sql
@@ -106,3 +106,48 @@ create table evento_reposicao(
     foreign key(tin) references(retalhista),
     primary key(ean, nro, num_serie, fabricante, instante),
 );
+
+----------------------------------------
+-- Populate Relations 
+----------------------------------------
+insert into categoria values ('Bolo');
+insert into categoria values ('Iogurte');
+insert into categoria values ('Salgado');
+insert into categoria values ('Refrigerante');
+insert into categoria values ('Água');
+insert into categoria values ('Bolacha');
+insert into categoria values ('Fruta');
+insert into categoria values ('Doce');
+insert into categoria values ('Bebida');
+
+insert into super_categoria values ('Doce');
+insert into super_categoria values ('Bebida');
+
+insert into categoria_simples values ('Bolo');
+insert into categoria_simples values ('Iogurte');
+insert into categoria_simples values ('Salgado');
+insert into categoria_simples values ('Refrigerante');
+insert into categoria_simples values ('Água');
+insert into categoria_simples values ('Bolacha');
+insert into categoria_simples values ('Fruta');
+
+insert into produto values (01234567890123, 'Bolo de Café', 'Bolo');
+insert into produto values (01234567890124, 'Fanta', 'Refrigerante');
+insert into produto values (01234567890125, 'Luso', 'Água');
+insert into produto values (01234567890126, 'Café', 'Bebida');
+
+insert into IVM values (1234, 'Repsol');
+insert into IVM values (2222, 'Empresa A');
+insert into IVM values (33, 'RNL');
+insert into IVM values (456, 'Diferencial');
+insert into IVM values (1, 'Sinfo');
+insert into IVM values (958, 'David Matos');
+
+insert into ponto_de_retalho values ("Ezequiel", "Faro", "Sé");
+insert into ponto_de_retalho values ("Ajdrúbal", "Santarém", "Almeirim");
+insert into ponto_de_retalho values ("Gertrudes", "Lisboa", "Arroios");
+
+insert into retalhista values (10, "Johnny Boy");
+insert into retalhista values (20, "Xalom");
+insert into retalhista values (30, "Nunca dá push");
+insert into retalhista values (40, "Carol");
