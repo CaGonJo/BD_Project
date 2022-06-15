3abc5eba7f28a0b310c4f28224f23b6a8da494fb
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-06-15 14:17:00 +0100

    Create base_de_dados.sql

---
+++ Parte3/base_de_dados.sql
@@ -0,0 +1,108 @@
+create table categoria (
+    nome    varchar(50),
+    primary key(nome)
+);
+
+create table categoria_simples (
+    nome    varchar(50),
+    foreign key(nome) references categoria(nome),
+    primary key(nome)
+);
+
+create table super_categoria (
+    nome    varchar(50),
+    foreign key(nome) references categoria(nome),
+    primary key(nome)
+);
+
+create table tem_outra (
+    nome_super_cat  varchar(50),
+    nome_cat    varchar(50),
+    foreign key(nome_super_cat) references super_categoria(nome),
+    foreign key(nome_cat) references categoria(nome),
+    primary key(nome_cat)
+);
+
+create table produto (
+    ean     numeric(13, 0),
+    descr   varchar(255),
+    cat     varchar(50),
+    primary key(ean),
+    foreign key(cat) references categoria(nome)
+);
+
+create table IVM (
+    num_serie   integer,
+    fabricante  varchar(50),
+    primary key (num_serie, fabricante)
+);
+
+create table ponto_de_retalho (
+    nome    varchar(50),
+    distrito    varchar(50),
+    concelho    varchar(50),
+    primary key(nome);
+);
+
+create table instalada_em (
+    num_serie   integer,
+    fabricante  varchar(50),
+    nome     varchar(50),
+    foreign key (num_serie, fabricante) references IVM(num_serie, fabricante),
+    foreign key (nome) references ponto_de_retalho(nome),
+    primary key (num_serie, fabricante)
+);
+
+create table prateleira (
+    nro     integer NOT NULL CHECK (nro >= 0),
+    num_serie   integer,
+    fabricante  varchar(50),
+    altura  integer NOT NULL CHECK (altura > 0),
+    nome    varchar(50),
+    foreign key(nome) references(categoria),
+    foreign key(num_serie, fabricante) references(IVM),
+    primary key(nro, num_serie, fabricante)
+);
+
+create table planograma (
+    ean     numeric(13, 0),
+    nro     integer,
+    num_serie   integer,
+    fabricante  varchar(50),
+    faces   integer CHECK (faces > 0),
+    unidades    integer NOT NULL CHECK (unidades > 0),
+    loc     integer NOT NULL CHECK (loc >= 0),
+    foreign key(ean) references(produto),
+    foreign key(nro, num_serie, fabricante) references(prateleira),
+    primary key(ean, nro, num_serie, fabricante)
+);
+
+create table retalhista (
+    tin     integer,
+    nome    varchar(50) NOT NULL UNIQUE,
+    primary key(tin)
+);
+
+create table responsavel_por (
+    nome_cat    varchar(50),
+    tin     integer,
+    num_serie   integer,
+    fabricante  varchar(50),
+    foreign key(num_serie, fabricante) references(IVM),
+    foreign key(tin) references(retalhista),
+    foreign key(nome_cat) references(categoria),
+    primary key(num_serie, fabricante)
+);
+
+create table evento_reposicao(
+    ean     numeric(13, 0),
+    nro     integer,
+    num_serie   integer,
+    fabricante  varchar(50),
+    instante    integer NOT NULL CHECK (instante >= 0),
+    unidades    integer NOT NULL CHECK (unidades > 0),
+    tin     integer,
+    foreign key(ean, nro, num_serie, fabricante) references(planograma),
+    foreign key(tin) references(retalhista),
+    primary key(ean, nro, num_serie, fabricante, instante),
+);
