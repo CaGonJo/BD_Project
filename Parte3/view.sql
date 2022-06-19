12d1d5b5f500a62db23e7f672c81f7db0bc10ed6
carolcoelho11 <81435821+carolcoelho11@users.noreply.github.com>
2022-06-19 15:23:41 +0100

    view done

---
+++ Parte3/view.sql
@@ -0,0 +1,13 @@
+
+create view vendas(ean,cat,ano,trimestre,mes,dia_mes,dia_semana,distrito,concelho,unidades) as 
+    select t.ean,t.cat,
+            EXTRACT(YEAR from e.instante),
+            EXTRACT(QUARTER from e.instante),
+            EXTRACT(MONTH from e.instante),
+            EXTRACT(DAY from e.instante),
+            EXTRACT(DOW from e.instante),
+            i.distrito,i.concelho,e.unidades
+    from ponto_de_retalho as p,
+    INNER JOIN instalada_em as i on i.nome=p.nome
+    INNER JOIN evento_reposicao as e on (e.num_serie=i.num_serie and e.fabricante LIKE i.fabricante)
+    INNER JOIN tem_categoria as t on (t.ean=e.ean)
