b0b04b573c69911d3eaf3012585b5b3bc2805a4d
carolcoelho11 <carol.coelho@sapo.pt>
2022-06-20 16:52:23 +0100

    OLAPS corrigidas

--- Parte3/analytics.sql
+++ Parte3/analytics.sql
@@ -1,12 +1,12 @@
-SELECT dia_semana,concelho, SUM (unidades) AS TOTQTY,
+SELECT dia_semana,concelho, SUM (unidades) AS TOTQTY
 FROM vendas
 WHERE ano BETWEEN 2016 and 2022
 GROUP BY
     GROUPING SETS ((dia_semana), (concelho), ());
 
 
-SELECT concelho,categir,dia_semana, SUM(unidades) AS TOTQTY,
+SELECT concelho,cat,dia_semana, SUM(unidades) AS TOTQTY
 FROM vendas
-WHERE distrito LIKE "Distrito_0"
+WHERE distrito='Distrito_0'
 GROUP BY
-    GROUPING SETS ((concelho), (categoria), (dia_semana), ())
+    GROUPING SETS ((concelho), (cat), (dia_semana), ())
--- Parte3/view.sql
+++ Parte3/view.sql
@@ -6,8 +6,8 @@ create view vendas(ean,cat,ano,trimestre,mes,dia_mes,dia_semana,distrito,concelh
             EXTRACT(MONTH from e.instante),
             EXTRACT(DAY from e.instante),
             EXTRACT(DOW from e.instante),
-            i.distrito,i.concelho,e.unidades
-    from ponto_de_retalho as p,
+            p.distrito,p.concelho,e.unidades
+    from ponto_de_retalho as p
     INNER JOIN instalada_em as i on i.nome=p.nome
     INNER JOIN evento_reposicao as e on (e.num_serie=i.num_serie and e.fabricante LIKE i.fabricante)
     INNER JOIN tem_categoria as t on (t.ean=e.ean)
