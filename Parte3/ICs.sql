c4ce40720754e5b9937206e45b4cc6b878e3a66e
carolcoelho11 <carol.coelho@sapo.pt>
2022-06-22 15:53:53 +0100

    olap 2 corrigida

--- Parte3/ICs.sql
+++ Parte3/ICs.sql
@@ -1,22 +1,24 @@
-create function num_unidades_permitido () returns trigger as $$
-    declare max_unid integer;
+create function num_unidades_permitido () returns trigger as 
+    declare  max_unid integer;
+    $$
     begin
         SELECT unidades into max_unid
         FROM planograma
-        WHERE planograma.ean=new.ean and plnograma.nro=new.nro and planograma.num_serie=new.num_serie and planograma.fabricante LIKE new.fabricante
+        WHERE planograma.ean=new.ean and planograma.nro=new.nro and planograma.num_serie=new.num_serie and planograma.fabricante LIKE new.fabricante
         
-        if new.unidades>max_unid then
+        if new.unidades > max_unid then
             raise exception 'O numero de unidades do evento de reposicao é superior ao permitido pelo planograma';
         end if;
         return new;
-    end;
+    end; 
+    $$ 
 
 create trigger verifica_unidades_reposicao before insert on evento_reposicao
 for each row execute procedure num_unidades_permitido();
 
 
 create function cat_prateleira () returns trigger as 
-    declare categoria varchar(50);
+    $$ declare categoria varchar(50);
     begin
 
         SELECT nome into categoria
@@ -32,6 +34,7 @@ create function cat_prateleira () returns trigger as
         end if;
         return new;
     end;
+    $$
 
 create trigger verifica_planograma before insert on planograma
 for each row execute procedure cat_prateleira();
--- Parte3/analytics.sql
+++ Parte3/analytics.sql
@@ -9,4 +9,4 @@ SELECT concelho,cat,dia_semana, SUM(unidades) AS TOTQTY
 FROM vendas
 WHERE distrito='Distrito_0'
 GROUP BY
-    GROUPING SETS ((concelho), (cat), (dia_semana), ())
+    GROUPING SETS ((concelho, cat, dia_semana), ())
