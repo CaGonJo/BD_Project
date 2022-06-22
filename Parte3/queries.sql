b49baba82ebf1efb427583e7ebe82bf5abc3ee49
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-06-22 20:38:28 +0100

    Removed WITH statement on queries 1 and 2, fixed query 3

--- Parte3/queries.sql
+++ Parte3/queries.sql
@@ -1,45 +1,24 @@
 --Qual o nome do retalhista (ou retalhistas) responsáveis pela reposição do maior número de categorias?
-WITH t as (
-    SELECT nome, contagem
-    FROM retalhista AS r NATURAL JOIN (
-        SELECT tin, COUNT(DISTINCT nome_cat) as contagem
-        FROM responsavel_por
-        GROUP BY tin
-    ) AS rp
-)
-SELECT t.nome
-FROM t
-WHERE t.contagem = (
-    SELECT MAX(t.contagem)
-    FROM t
-    );
+SELECT t2.nome
+FROM (retalhista AS r INNER JOIN responsavel_por AS rp ON r.tin = rp.tin) AS t2
+GROUP BY t2.nome
+HAVING COUNT(DISTINCT t2.nome_cat) = (SELECT MAX(contagem)
+                                      FROM (SELECT COUNT(DISTINCT nome_cat) AS contagem                                                                                       FROM responsavel_por) as t1);
 
 --Qual o nome do ou dos retalhistas que são responsáveis por todas as categorias simples?
-
-WITH t as (
-    SELECT nome, contagem
-    FROM retalhista NATURAL JOIN (
-        SELECT tin, COUNT(DISTINCT rp.nome_cat) as contagem
-        FROM responsavel_por as rp, categoria_simples as cs
-        WHERE rp.nome_cat in cs.nome
-        GROUP BY tin
-    )
-)
-
-SELECT r.nome
-FROM retalhista as r
-WHERE (
-    SELECT COUNT(DISTINCT nome) 
-    FROM categoria_simples) = (
-
-    SELECT contagem 
-    FROM t
-    WHERE r.nome=t.nome);
+SELECT t1.nome
+FROM (retalhista AS r INNER JOIN (SELECT tin, nome_cat 
+                                  FROM responsavel_por
+                                  WHERE nome_cat IN (SELECT nome
+                                                     FROM categoria_simples)) AS rp 
+      ON r.tin = rp.tin) AS t1
+GROUP BY t1.nome
+HAVING COUNT(DISTINCT t1.nome_cat) = (SELECT COUNT(nome) FROM categoria_simples);
 --Quais os produtos (ean) que nunca foram repostos?
 
 SELECT ean
 FROM produto
-WHERE ean NOT IN (SELECT ean FROM planograma);
+WHERE ean NOT IN (SELECT ean FROM evento_de_reposicao);
 
 -- Quais os produtos (ean) que foram repostos sempre pelo mesmo retalhista?
 SELECT ean
