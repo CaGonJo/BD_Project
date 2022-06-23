d153dc353eaa3b9d6aba11cdff5011fa863e1a8f
carolcoelho11 <carol.coelho@sapo.pt>
2022-06-23 22:09:56 +0100

    Parte do Relatorio da Carolina done

--- Parte3/view.sql
+++ Parte3/view.sql
@@ -1,5 +1,6 @@
 
-create view vendas(ean,cat,ano,trimestre,mes,dia_mes,dia_semana,distrito,concelho,unidades) as 
+create view vendas(ean,cat,ano,trimestre,mes,dia_mes,
+                dia_semana,distrito,concelho,unidades) as 
     select t.ean,t.cat,
             EXTRACT(YEAR from e.instante),
             EXTRACT(QUARTER from e.instante),
@@ -9,5 +10,6 @@ create view vendas(ean,cat,ano,trimestre,mes,dia_mes,dia_semana,distrito,concelh
             p.distrito,p.concelho,e.unidades
     from ponto_de_retalho as p
     INNER JOIN instalada_em as i on i.nome=p.nome
-    INNER JOIN evento_reposicao as e on (e.num_serie=i.num_serie and e.fabricante LIKE i.fabricante)
+    INNER JOIN evento_reposicao as e on (e.num_serie=i.num_serie and 
+                                        e.fabricante LIKE i.fabricante)
     INNER JOIN tem_categoria as t on (t.ean=e.ean)
